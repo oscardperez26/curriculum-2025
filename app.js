f6f2b8e1c22ba191bd0016ddcafdd3b7de055103
danielunfit <odanieloperez26@hotmail.com>
2025-03-25 16:06:37 -0500

    agregue aqui mis certificaciones

--- app.js
+++ app.js
@@ -1,18 +1,32 @@
 let menuVisible = false;
 //Función que oculta o muestra el menu
-function mostrarmenuoculto(){
-    if(menuVisible){
-        document.getElementById("nav").classList ="";
-        menuVisible = false;
-    }else{
-        document.getElementById("nav").classList ="responsive";
-        menuVisible = true;
-    }
-}
-
-
-function seleccionar(){
-    //oculto el menu una vez que selecciono una opcion
+function mostrarmenuoculto() {
+  if (menuVisible) {
     document.getElementById("nav").classList = "";
     menuVisible = false;
+  } else {
+    document.getElementById("nav").classList = "responsive";
+    menuVisible = true;
+  }
 }
+
+function seleccionar() {
+  //oculto el menu una vez que selecciono una opcion
+  document.getElementById("nav").classList = "";
+  menuVisible = false;
+}
+/**slider ceritficaicones */
+let index = 0;
+
+function moveSlide(step) {
+  const slides = document.querySelector(".slides");
+  const totalSlides = document.querySelectorAll(".slide").length;
+
+  index += step;
+  if (index < 0) index = totalSlides - 1;
+  if (index >= totalSlides) index = 0;
+
+  slides.style.transform = `translateX(${-index * 100}%)`;
+}
+
+setInterval(() => moveSlide(1), 3000);
